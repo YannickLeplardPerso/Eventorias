b76fe81ddd005ed8e4d46e94183d77459dff9ccb
Yannick Leplard <leplard.yannick@gmail.com>
2024-11-10 16:49:08 +0100

    Initial Commit

---
+++ EventoriasTests/EventoriasTests.swift
@@ -0,0 +1,17 @@
+//
+//  EventoriasTests.swift
+//  EventoriasTests
+//
+//  Created by Yannick LEPLARD on 10/11/2024.
+//
+
+import Testing
+@testable import Eventorias
+
+struct EventoriasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
